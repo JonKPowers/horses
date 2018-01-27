eea7cd89d6b098e0eb85b9f8841ba3a1d487b987
Jon Powers <Jon.K.Powers+git@gmail.com>
2018-01-26 22:12:25 -0500

    Work on database functions.

--- database_functions.py
+++ database_functions.py
@@ -1,13 +1,51 @@
 import pymysql.cursors
 
 
-class DbHandler:
-    def __init__(self, db='horses_test', table=None, columns=None, **kwargs):
+class DbUpdater:
+    def __init__(self, db='horses_test', name_files=None):
         self.db = db
-        self.table = table
-        self.columns = columns
         self.mysql = None
+
+    def initialize_db(self):
+        # Check to see if db exists. If not, create it.
         pass
+
+    def initialize_table(self, table, table_type, table_data):
+        # Check to see if table exists and whether it has the right columns
+        # If the table exists, let us know. Otherwise, create the table
+        # Figure out correct columns from table_type argument
+        table_exists = None
+        try:
+            with self.mysql.cursor() as cursor:
+                sql = "SELECT count(*) FROM information_schema.TABLES " \
+                      "WHERE (TABLE_SCHEMA = '{}') AND (TABLE_NAME = '{}')".format(
+                                                                            self.db, table
+                                                                            )
+                cursor.execute(sql)
+                table_exists = [item for item in cursor][0][0]
+
+                if table_exists:
+                    print("Table {} already exists--skipping creation step.".format(table))
+                elif table_exists is None:
+                    print("There was an error determining if table {} exists--skipping creation step.".format(table))
+                elif table_exists == 0:
+                    self.__create_table(cursor, table, table_data)
+                else:
+                    print("There was a problem checking whether {} exists--table_exists still at default.".format(table))
+        finally:
+            self.mysql.close()
+
+
+    def add_to_table(self, table, table_data):
+        # Run some checks to see if data looks like the right shape.
+        # Add each row of data to the table
+        # Add some logging?
+        columns = [key for key, value in table_data.dtypes.items()]
+        try:
+            with self.mysql.cursor() as cursor:
+                self.__insert_records(cursor, table, table_data, columns)
+        finally:
+            self.mysql.close()
 
     # Hidden methods ------------------------
 
@@ -18,34 +56,47 @@ class DbHandler:
             password='ABCabc123!'
         )
 
-    def __create_db(self, db_connection):
+    def __create_db(self, cursor):
         sql = 'CREATE DATABASE {}'.format(self.db)
-        db_connection.execute(sql)
+        cursor.execute(sql)
         self.mysql.commit()
 
-    def __use_db(self, db_connection):
+    def __use_db(self, cursor):
         sql = 'USE {}'.format(self.db)
-        db_connection.execute(sql)
+        cursor.execute(sql)
         self.mysql.commit()
 
-    def __create_table(self, db_connection):
-        sql = "CREATE TABLE '{}' (".format(self.table)
-        # CODE TO FIGURE OUT AND ENUMERATE COLUMNS AND TYPES
-        # for i in range(len(self.columns)-1):
-        #     column = self.columns[i]
-        #     sql += "'{}' char(32), ".format(column)
-        # sql += "'{}' char(32))".format(self.columns[-1])
-        db_connection(sql)
+    def __create_table(self, cursor, table, table_data):
+        data_type = None
+        sql = "CREATE TABLE '{}' (".format(table)
+        for key, value in table_data.dtypes.items():
+            if value == 'object':
+                data_type = 'VARCHAR(255)'
+            elif value == 'int64':
+                data_type = 'INT'
+            elif value =='float64':
+                data_type = 'FLOAT'
+            else:
+                print('Error determining datatype for key: {}, value: {}'.format(key, value))
+            sql += "{} {}, ".format(key, data_type)
+        sql = sql[:-2]      # Chop off extra ', ' ...
+        sql += ')'          # ... and balance parentheses before sending.
+        cursor.execute(sql)
         self.mysql.commit()
+        #   Need to add primary/unique key constraints.
+        #   Maybe pull those 'extras' out from a separate function that
+        #   returns the extra stuff based on the table_
 
-    def insert_record(self, db_connection):
-        # CODE TO CYCLE THROUGH RECORDS TO INSERT
-        # for mushroom in self.mushrooms:
-        #     values = mushroom.harvest(self.columns)
-        #     sql = "INSERT INTO {} ('{}') VALUE('{}')".format(
-        #         self.table,
-        #         "', '".join(self.columns),
-        #         "', '".join(values)
-        #     )
-        #    db_connection.execute(sql)
+    def __insert_records(self, cursor, table, table_data, columns):
+        table_values = table_data.values
+        for i in range(len(columns)):
+            values_string = ""
+            for item in table_values[i]:
+                values_string += str(item) + "', '"
+            sql = "INSERT INTO {} ('{}') VALUES('{}')".format(
+                                                        table,
+                                                        ", ".join(columns),
+                                                        values_string
+                                                        )
+            cursor.execut(sql)
         self.mysql.commit()
