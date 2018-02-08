d36ef114fc2452c3435babc0b0b0fdc6e105d8ed
Jon Powers <Jon.K.Powers+git@gmail.com>
2018-02-08 06:32:52 -0500

    refactored preprocessing code to make more concise

--- df_preprocessing.py
+++ df_preprocessing.py
@@ -7,18 +7,18 @@ def tidy_it_up(table_data, extension):
         # *********************************************
         # Flag fixes
 
-        table_data['about_dist_flag'] = table_data['about_dist_flag'].replace('A', 1)
+        table_data['about_dist_flag'].replace('A', 1, inplace=True)
 
-        table_data['all_weather_flag'] = table_data['all_weather_flag'].replace('A', 1)
-        table_data['all_weather_flag'] = table_data['all_weather_flag'].fillna(0)
+        table_data['all_weather_flag'].replace('A', 1, inplace=True)
+        table_data['all_weather_flag'].fillna(0)
 
-        table_data['chute_start_flag'] = table_data['chute_start_flag'].replace('C', 1)
-        table_data['chute_start_flag'] = table_data['chute_start_flag'].replace('N', 0)
-        table_data['chute_start_flag'] = table_data['chute_start_flag'].fillna(0)
+        table_data['chute_start_flag'].replace('C', 1, inplace=True)
+        table_data['chute_start_flag'].replace('N', 0, inplace=True)
+        table_data['chute_start_flag'].fillna(0)
 
-        table_data['off_turf_flag'] = table_data['off_turf_flag'].replace('O', 1)
-        table_data['off_turf_dist_change'] = table_data['off_turf_dist_change'].replace('Y', 1)
-        table_data['off_turf_dist_change'] = table_data['off_turf_dist_change'].replace('N', 0)
+        table_data['off_turf_flag'].replace('O', 1, inplace=True)
+        table_data['off_turf_dist_change'].replace('Y', 1, inplace=True)
+        table_data['off_turf_dist_change'].replace('N', 0, inplace=True)
 
         # *********************************************
         # Fill and na's with NULL for MYSQL transfer
@@ -27,17 +27,17 @@ def tidy_it_up(table_data, extension):
     if extension == '2':
         print('Tidying .2 file ...')
 
-        table_data['nonbetting_flag'] = table_data['nonbetting_flag'].replace('Y', 1)
-        table_data['nonbetting_flag'] = table_data['nonbetting_flag'].fillna(0)
+        table_data['nonbetting_flag'].replace('Y', 1, inplace=True)
+        table_data['nonbetting_flag'].fillna(0, inplace=True)
 
-        table_data['disqualified_flag'] = table_data['disqualified_flag'].replace('Y', 1)
+        table_data['disqualified_flag'].replace('Y', 1, inplace=True)
 
-        table_data['corrected_weight'] = table_data['corrected_weight'].replace('Y', 1)
-        table_data['corrected_weight'] = table_data['corrected_weight'].fillna(0)
+        table_data['corrected_weight'].replace('Y', 1, inplace=True)
+        table_data['corrected_weight'].fillna(0, inplace=True)
 
-        table_data['claimed_flag'] = table_data['claimed_flag'].replace('Y', 1)
-        table_data['claimed_flag'] = table_data['claimed_flag'].replace('N', 0)
-        table_data['dead_heat_flag'] = table_data['dead_heat_flag'].replace('DH', 1)
+        table_data['claimed_flag'].replace('Y', 1, inplace=True)
+        table_data['claimed_flag'].replace('N', 0, inplace=True)
+        table_data['dead_heat_flag'].replace('DH', 1, inplace=True)
 
 
 
@@ -49,7 +49,7 @@ def tidy_it_up(table_data, extension):
         # *********************************************
         # Character fields to make null
 
-        table_data['claimed_trainer_middle'] = table_data['claimed_trainer_middle'].fillna('NULL')
+        table_data['claimed_trainer_middle'].fillna('NULL', inplace=True)
 
         # **********************************************
         # FIELD DTYPES TO FIX
@@ -58,7 +58,7 @@ def tidy_it_up(table_data, extension):
 
         # **********************************************
         # Fill the rest with 'NULL'
-        table_data = table_data.fillna('NULL')
+        table_data = table_data.fillna('NULL', inplace=True)
 
     if extension == '3':
         print('Tidying .3 file (ITM payoffs) ...')
@@ -78,7 +78,7 @@ def tidy_it_up(table_data, extension):
 
         # **********************************************
         # Fill the rest with 'NULL'
-        table_data = table_data.fillna('NULL')
+        table_data = table_data.fillna('NULL', inplace=True)
 
     if extension == '4':
         print('Tidying .4 file (exotic payoffs) ...')
@@ -98,7 +98,7 @@ def tidy_it_up(table_data, extension):
 
         # **********************************************
         # Fill the rest with 'NULL'
-        table_data = table_data.fillna('NULL')
+        table_data.fillna('NULL', inplace=True)
 
     if extension == '5':
         print('Tidying .5 file (breeding info)...')
@@ -118,7 +118,7 @@ def tidy_it_up(table_data, extension):
 
         # **********************************************
         # Fill the rest with 'NULL'
-        table_data = table_data.fillna('NULL')
+        table_data.fillna('NULL', inplace=True)
 
     if extension == '6':
         print('Tidying .6 file (footnotes) ...')
@@ -135,7 +135,7 @@ def tidy_it_up(table_data, extension):
 
         # **********************************************
         # Fill the rest with 'NULL'
-        table_data = table_data.fillna('NULL')
+        table_data.fillna('NULL', inplace=True)
 
     if extension == 'DRF':
         print('Tidying .DRF file (past performances) ...')
@@ -152,49 +152,47 @@ def tidy_it_up(table_data, extension):
         # *********************************************
         # Field with [value]/NaN flags
 
-        table_data['apprentice_wgt_alw'] = table_data['apprentice_wgt_alw'].fillna(0)
+        table_data['apprentice_wgt_alw'].fillna(0, inplace=True)
 
-        table_data['statebread_flag'] = table_data['statebread_flag'].replace('s', 1)
-        table_data['today_nasal_strip_chg'] = table_data['today_nasal_strip_chg'].replace(9, 'NULL')
-        table_data['todays_meds_new'] = table_data['todays_meds_new'].replace(9, 'NULL')
-        table_data['todays_meds_old'] = table_data['todays_meds_old'].replace(9, 'NULL')
-        table_data['equipment_change'] = table_data['equipment_change'].replace(9, 'NULL')
-        table_data['allweather_surface'] = table_data['allweather_surface'].replace('A', 1)
-
-        for i in range(1, 11):
-            table_data['past_special_chute_{}'.format(i)] = table_data['past_special_chute_{}'.format(i)].replace('c', 1)
-            table_data['past_special_chute_{}'.format(i)] = table_data['past_special_chute_{}'.format(i)].fillna(0)
+        table_data['statebread_flag'].replace('s', 1, inplace=True)
+        table_data['today_nasal_strip_chg'].replace(9, 'NULL', inplace=True)
+        table_data['todays_meds_new'].replace(9, 'NULL', inplace=True)
+        table_data['todays_meds_old'].replace(9, 'NULL', inplace=True)
+        table_data['equipment_change'].replace(9, 'NULL', inplace=True)
+        table_data['allweather_surface'].replace('A', 1, inplace=True)
 
         for i in range(1, 11):
-            table_data['past_bar_shoe_{}'.format(i)] = table_data['past_bar_shoe_{}'.format(i)].replace('r', 1)
-            table_data['past_bar_shoe_{}'.format(i)] = table_data['past_bar_shoe_{}'.format(i)].fillna(0)
+            table_data['past_special_chute_{}'.format(i)].replace('c', 1, inplace=True)
+            table_data['past_special_chute_{}'.format(i)].fillna(0, inplace=True)
 
         for i in range(1, 11):
-            table_data['past_sealed_track_indicator_{}'.format(i)] = \
-                table_data['past_sealed_track_indicator_{}'.format(i)].replace('s', 1)
-            table_data['past_sealed_track_indicator_{}'.format(i)] = \
-                table_data['past_sealed_track_indicator_{}'.format(i)].fillna(0)
+            table_data['past_bar_shoe_{}'.format(i)].replace('r', 1, inplace=True)
+            table_data['past_bar_shoe_{}'.format(i)].fillna(0, inplace=True)
 
         for i in range(1, 11):
-            table_data['past_all_weather_flag_{}'.format(i)] = table_data['past_all_weather_flag_{}'.format(i)].replace('A', 1)
-            table_data['past_all_weather_flag_{}'.format(i)] = table_data['past_all_weather_flag_{}'.format(i)].fillna(0)
+            table_data['past_sealed_track_indicator_{}'.format(i)].replace('s', 1, inplace=True)
+            table_data['past_sealed_track_indicator_{}'.format(i)].fillna(0, inplace=True)
 
         for i in range(1, 11):
-            table_data['past_equipment_{}'.format(i)] = table_data['past_equipment_{}'.format(i)].replace('b', 1)
-            table_data['past_equipment_{}'.format(i)] = table_data['past_equipment_{}'.format(i)].fillna(0)
+            table_data['past_all_weather_flag_{}'.format(i)].replace('A', 1, inplace=True)
+            table_data['past_all_weather_flag_{}'.format(i)].fillna(0, inplace=True)
 
         for i in range(1, 11):
-            table_data['past_entry_{}'.format(i)] = table_data['past_entry_{}'.format(i)].replace('e', 1)
-            table_data['past_entry_{}'.format(i)] = table_data['past_entry_{}'.format(i)].fillna(0)
+            table_data['past_equipment_{}'.format(i)].replace('b', 1, inplace=True)
+            table_data['past_equipment_{}'.format(i)].fillna(0, inplace=True)
 
         for i in range(1, 11):
-            table_data['past_claimed_code_{}'.format(i)] = table_data['past_claimed_code_{}'.format(i)].replace('c', 1)
-            table_data['past_claimed_code_{}'.format(i)] = table_data['past_claimed_code_{}'.format(i)].replace('v', 1)
-            table_data['past_claimed_code_{}'.format(i)] = table_data['past_claimed_code_{}'.format(i)].fillna(0)
+            table_data['past_entry_{}'.format(i)].replace('e', 1, inplace=True)
+            table_data['past_entry_{}'.format(i)].fillna(0, inplace=True)
 
         for i in range(1, 11):
-            table_data['past_statebred_flag_{}'.format(i)] = table_data['past_statebred_flag_{}'.format(i)].replace('s', 1)
-            table_data['past_statebred_flag_{}'.format(i)] = table_data['past_statebred_flag_{}'.format(i)].fillna(0)
+            table_data['past_claimed_code_{}'.format(i)].replace('c', 1, inplace=True)
+            table_data['past_claimed_code_{}'.format(i)].replace('v', 1, inplace=True)
+            table_data['past_claimed_code_{}'.format(i)].fillna(0, inplace=True)
+
+        for i in range(1, 11):
+            table_data['past_statebred_flag_{}'.format(i)].replace('s', 1, inplace=True)
+            table_data['past_statebred_flag_{}'.format(i)].fillna(0, inplace=True)
 
 
 
@@ -208,6 +206,6 @@ def tidy_it_up(table_data, extension):
 
         # **********************************************
         # Fill the rest with 'NULL'
-        table_data = table_data.fillna('NULL')
+        table_data.fillna('NULL', inplace=True)
 
     return table_data
