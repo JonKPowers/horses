8661a36cf6a1ac3c3de23ae8d1fe7b4db9fa8299
Jon Powers <Jon.K.Powers+git@gmail.com>
2018-03-01 06:05:12 -0500

    commit

---
+++ race_class_data_aggregator.py
@@ -0,0 +1,106 @@
+race_class_data = {
+    'track': {
+        'race_general_results': 'track',
+        'race_info': 'track',
+        'horse_pps': 'track_code'       # I assume track_code always == track_code_BRIS
+    },
+
+    'date': {
+        'race_general_results': 'date',
+        'race_info': 'date',
+        'horse_pps': 'race_date',
+    },
+
+    'race_num': {
+        'race_general_results': 'race_num',
+        'race_info': 'race_num',
+        'horse_pps': 'race_num',
+    },
+
+    'time_local': {
+        # *******TO DO **************
+    },
+
+    'temperature': {
+        'horses_general_results': 'race_temp',
+    },
+    'weather': {
+        'horses_general_results': 'weather',
+    },
+
+    'distance': {
+        'horses_general_results': 'distance',
+        'race_info': None,
+        'horse_pps': None,
+    },
+
+    'run_up_distance': {
+        'horses_general_results': 'run_up_dist',
+
+    },
+
+    'temp_rail_distance': {
+        'horses_general_results': 'temp_rail_dist',
+
+    },
+
+    'about_distance_flag': {
+        'horses_general_results': 'about_distance',
+    },
+
+    'surface': {
+        'horses_general_results': 'surface_new',
+    },
+
+    'track_condition': {
+        'horses_general_results': 'track_condition',
+    },
+
+    'chute_start': {
+        'horses_general_results': 'chute_start',
+    },
+
+    'off_turf': {
+        'horses_general_results': 'off_turf',
+    },
+
+    'off_turf_dist_change': {
+        'horses_general_results': 'off_turf_distance_change',
+    },
+
+    'num_of_horses': {
+        'horses_general_results': 'field_size',
+    },
+
+    'breed': {
+
+    },
+    'ages_allowed': {
+        'two': {},
+        'three': {},
+        'four': {},
+        'five': {},
+        'older': {},
+    },
+    'sexes_allowed': {
+        'fillies': {},
+        'mares': {},
+        'colts_geldings': {},
+    },
+
+    'statebred_race': {},
+    'not_won_limit': {},
+    'money_limit': {},
+    'time_limit': {},
+
+    'purse': {},
+    'claiming_price_base': {},
+
+    'race_type': {},
+    'race_grade': {},
+
+    'race_conditions': {},
+    'race_notes': {},
+
+
+}
